21532a20389b20c619b99b96091b6fd2211c0b2f
lara-ferrer <larafpics@gmail.com>
2021-10-02 23:47:29 +0200

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import "./header.scss"
 import logo from "../../images/logo.svg";
-
+import scrollTo from 'gatsby-plugin-smoothscroll';
 export default function Header() {
     return (
         <div className="container m-header grid-30-70">
             <img src={logo} alt="Plant a tomato" className="a-logo"/>
             <ul>
                 <li className="a-menu__item">
-                    <a>Solar radiation</a>
+                    <a onClick={() => scrollTo('#solar-radiation')}>Solar radiation</a>
                 </li>
                 <li className="a-menu__item">
-                    <a>Discover radiation levels</a>
+                    <a onClick={() => scrollTo('#benefits')}>Discover radiation levels</a>
+                </li>
+                <li className="a-menu__item">
+                    <a className="a-button">Get our app</a>
                 </li>
             </ul>
         </div>
