3567a126a1344847bbd3fecaa3ff5f7c0b38c2a3
lara-ferrer <larafpics@gmail.com>
2021-10-03 02:01:34 +0200

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,22 +29,5 @@ module.exports = {
         display: 'swap'
       }
     },
-    {
-      resolve: 'gatsby-plugin-favicons',
-      options: {
-        logo: './src/images/favicon.png',
-        appName: 'Plant a Tomato',
-        background: '#fff',
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          yandex: false,
-          windows: false
-        }
-      }
-    }
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,15 @@ import Benefits from "../components/Benefits";
 import AppInfo from "../components/AppInfo";
 import Footer from "../components/Footer";
 import ButtonUp from "../components/ButtonUp";
+import Helmet from 'react-helmet'
+import favicon from "../images/favicon.png";
 
 const IndexPage = () => {
   return (
     <div>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <title>Planting Tomatoes</title>
       <Header />
       <Hero />
